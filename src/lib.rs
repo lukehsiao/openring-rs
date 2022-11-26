3ddbcd801166c9b33385027a7042c66971ca899a
Luke Hsiao <luke@hsiao.dev>
2022-11-26 11:32:08 -0700

    fix: properly decode html entities
    Rather than a naive replace.

--- src/lib.rs
+++ src/lib.rs
@@ -167,12 +167,17 @@ pub fn run(args: Args) -> Result<()> {
                                 }
                             },
                         };
-                        let safe_summary = ammonia::clean(&summary).replace("&nbsp;", "");
+                        let mut safe_summary = String::new();
+                        html_escape::decode_html_entities_to_string(
+                            ammonia::clean(&summary),
+                            &mut safe_summary,
+                        );
+                        warn!("summary: {:#?}", safe_summary);
                         articles.push(Article {
                             link: Url::parse(link)
                                 .with_context(|| format!("Unabled to parse url `{}`", c.link()))?,
                             title: title.to_string(),
-                            summary: safe_summary,
+                            summary: safe_summary.to_string(),
                             source_link: source_link.clone(),
                             source_title: source_title.clone(),
                             date: date
@@ -212,7 +217,12 @@ pub fn run(args: Args) -> Result<()> {
                                     }
                                 },
                             };
-                            let safe_summary = ammonia::clean(&summary).replace("&nbsp;", "");
+                            let mut safe_summary = String::new();
+                            html_escape::decode_html_entities_to_string(
+                                ammonia::clean(&summary),
+                                &mut safe_summary,
+                            );
+                            warn!("summary: {:#?}", safe_summary);
                             // Uses the last link, since blogspot puts the article link last.
                             let link = Url::parse(
                                 item.links()
@@ -227,7 +237,7 @@ pub fn run(args: Args) -> Result<()> {
                             articles.push(Article {
                                 link,
                                 title: item.title().to_string(),
-                                summary: safe_summary,
+                                summary: safe_summary.to_string(),
                                 source_link: source_link.clone(),
                                 source_title: source_title.clone(),
                                 date: item
