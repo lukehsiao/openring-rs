c4b8055c89ea499f67b1f0ff08b9f04e64056702
Luke Hsiao <luke@hsiao.dev>
2023-08-10 13:25:13 -0600

    feat: provide `miette`-powered error diagnostics
    While we're at it, refactor the code into more reasonable functions.
    
    Also switches to use more structured logging.
    
    Signed-off-by: Luke Hsiao <luke@hsiao.dev>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use anyhow::Result;
-
 use clap::Parser;
+use miette::{Context, Result};
 
 use openring::{self, Args};
 
@@ -8,19 +7,22 @@ fn main() -> Result<()> {
     let args = Args::parse();
 
     tracing_subscriber::fmt()
-        .with_env_filter(convert_filter(args.verbose.log_level_filter()))
+        .with_env_filter(format!(
+            "openring={},html5ever=off,ureq=off",
+            convert_filter(args.verbose.log_level_filter())
+        ))
         .init();
-    openring::run(args)
+    // I feel like I shouldn't need wrap_err, but it doesn't work without it.
+    openring::run(args).wrap_err("runtime error")
 }
 
-fn convert_filter(filter: log::LevelFilter) -> String {
-    let filter = match filter {
-        log::LevelFilter::Off => "off",
-        log::LevelFilter::Error => "error",
-        log::LevelFilter::Warn => "warn",
-        log::LevelFilter::Info => "info",
-        log::LevelFilter::Debug => "debug",
-        log::LevelFilter::Trace => "trace",
-    };
-    format!("openring={},html5ever=off,ureq=off", filter)
+fn convert_filter(filter: log::LevelFilter) -> tracing_subscriber::filter::LevelFilter {
+    match filter {
+        log::LevelFilter::Off => tracing_subscriber::filter::LevelFilter::OFF,
+        log::LevelFilter::Error => tracing_subscriber::filter::LevelFilter::ERROR,
+        log::LevelFilter::Warn => tracing_subscriber::filter::LevelFilter::WARN,
+        log::LevelFilter::Info => tracing_subscriber::filter::LevelFilter::INFO,
+        log::LevelFilter::Debug => tracing_subscriber::filter::LevelFilter::DEBUG,
+        log::LevelFilter::Trace => tracing_subscriber::filter::LevelFilter::TRACE,
+    }
 }
