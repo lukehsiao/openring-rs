lukehsiao/openring-rs

commit f85009b14763098692bb682e2c51f6bcd9f8b5b3

feat: support feeds with relative URLs
This patch adds support for relative URLs in feeds. For example,

    <rss version="2.0">
      <channel>
      <title/>
      <link>/</link>
      <description/>
      <generator>Hugo -- gohugo.io</generator>
      <language>en</language>
      <lastBuildDate>Tue, 10 Oct 2023 13:37:45 -0400</lastBuildDate>
      <atom:link href="/index.xml" rel="self" type="application/rss+xml"/>
      <item>
        <title>Things Fall Apart</title>
        <link>/posts/things-fall-apart/</link>
        <pubDate>Sat, 23 Sep 2023 06:17:10 -0500</pubDate>
        <guid>/posts/things-fall-apart/</guid>
      </item>
    ...

Feeds like this will now work.

Tested: The feed in [[1]] contains relative links, and parses correctly
  after this patch.

[1]: https://uptointerpretation.com/index.xml

Signed-off-by: Luke Hsiao <luke@hsiao.dev>

src/lib.rs
@@ -18,7 +18,7 @@ use tera::Tera;
 use thiserror::Error;
 use tracing::{debug, warn};
 use ureq::{Agent, AgentBuilder};
-use url::Url;
+use url::{ParseError, Url};
 
 type Result<T> = result::Result<T, OpenringError>;
 
@@ -255,14 +255,30 @@ pub fn run(args: Args) -> Result<()> {
                     None => {
                         // If an alternate link is missing just grab one of them
                         match feed.links.into_iter().next().map(|l| l.href) {
-                            Some(s) => Url::parse(&s)?,
+                            Some(s) => match Url::parse(&s) {
+                                Ok(u) => u,
+                                Err(ParseError::RelativeUrlWithoutBase) => {
+                                    Url::parse(&format!("{}{}", url, &s))?
+                                }
+                                Err(e) => return Err(OpenringError::UrlParseError(e)),
+                            },
                             None => return Err(OpenringError::FeedBadTitle(url.to_string())),
                         }
                     }
-                    Some(s) => Url::parse(s)?,
+                    Some(s) => match Url::parse(s) {
+                        Ok(u) => u,
+                        Err(ParseError::RelativeUrlWithoutBase) => {
+                            Url::parse(&format!("{}{}", url, &s))?
+                        }
+                        Err(e) => return Err(OpenringError::UrlParseError(e)),
+                    },
                 }
             }
-            Some(s) => Url::parse(s)?,
+            Some(s) => match Url::parse(s) {
+                Ok(u) => u,
+                Err(ParseError::RelativeUrlWithoutBase) => Url::parse(&format!("{}{}", url, &s))?,
+                Err(e) => return Err(OpenringError::UrlParseError(e)),
+            },
         };
         for entry in entries.iter() {
             if let (Some(link), Some(title), Some(date)) = (
@@ -278,11 +294,23 @@ pub fn run(args: Args) -> Result<()> {
                     })
                     .map(|l| &l.href)
                 {
-                    Some(s) => Url::parse(s).ok(),
+                    Some(s) => match Url::parse(s) {
+                        Ok(u) => Some(u),
+                        Err(ParseError::RelativeUrlWithoutBase) => {
+                            Url::parse(&format!("{}{}", url, &s)).ok()
+                        }
+                        Err(_) => None,
+                    },
                     None => {
                         // If an alternate link is missing just grab one of them
                         match entry.links.clone().into_iter().next().map(|l| l.href) {
-                            Some(s) => Url::parse(&s).ok(),
+                            Some(s) => match Url::parse(&s) {
+                                Ok(u) => Some(u),
+                                Err(ParseError::RelativeUrlWithoutBase) => {
+                                    Url::parse(&format!("{}{}", url, &s)).ok()
+                                }
+                                Err(_) => None,
+                            },
                             None => return Err(OpenringError::FeedBadTitle(url.to_string())),
                         }
                     }

commit a73455cb14831c3834d8c538949bf698b8296b7b

fix: make relative urls relative to origin
This fixes commit f85009b14763 ("feat: support feeds with relative
URLs"), which added support for relative URLs, but made them relative to
the URL of the feed itself, rather than the origin of the URL.

Before this patch, URLs might be resolved like:

    https:&#x2F;&#x2F;uptointerpretation.com&#x2F;index.xml&#x2F;posts&#x2F;how-big-could-rust-be&#x2F;

After this patch, they are correctly resovled as:

    https:&#x2F;&#x2F;uptointerpretation.com&#x2F;posts&#x2F;how-big-could-rust-be&#x2F;

Fixes: f85009b14763 ("feat: support feeds with relative URLs")
Signed-off-by: Luke Hsiao <luke@hsiao.dev>

src/lib.rs
@@ -255,20 +255,22 @@ pub fn run(args: Args) -> Result<()> {
                     None => {
                         // If an alternate link is missing just grab one of them
                         match feed.links.into_iter().next().map(|l| l.href) {
-                            Some(s) => match Url::parse(&s) {
-                                Ok(u) => u,
-                                Err(ParseError::RelativeUrlWithoutBase) => {
-                                    Url::parse(&format!("{}{}", url, &s))?
+                            Some(s) => {
+                                match Url::parse(&s) {
+                                    Ok(u) => u,
+                                    Err(ParseError::RelativeUrlWithoutBase) => Url::parse(
+                                        &format!("{}{}", url.origin().ascii_serialization(), &s),
+                                    )?,
+                                    Err(e) => return Err(OpenringError::UrlParseError(e)),
                                 }
-                                Err(e) => return Err(OpenringError::UrlParseError(e)),
-                            },
+                            }
                             None => return Err(OpenringError::FeedBadTitle(url.to_string())),
                         }
                     }
                     Some(s) => match Url::parse(s) {
                         Ok(u) => u,
                         Err(ParseError::RelativeUrlWithoutBase) => {
-                            Url::parse(&format!("{}{}", url, &s))?
+                            Url::parse(&format!("{}{}", url.origin().ascii_serialization(), &s))?
                         }
                         Err(e) => return Err(OpenringError::UrlParseError(e)),
                     },
@@ -276,48 +278,54 @@ pub fn run(args: Args) -> Result<()> {
             }
             Some(s) => match Url::parse(s) {
                 Ok(u) => u,
-                Err(ParseError::RelativeUrlWithoutBase) => Url::parse(&format!("{}{}", url, &s))?,
+                Err(ParseError::RelativeUrlWithoutBase) => {
+                    Url::parse(&format!("{}{}", url.origin().ascii_serialization(), &s))?
+                }
                 Err(e) => return Err(OpenringError::UrlParseError(e)),
             },
         };
         for entry in entries.iter() {
-            if let (Some(link), Some(title), Some(date)) = (
-                match entry
-                    .links
-                    .iter()
-                    .find(|l| {
-                        if let Some(rel) = &l.rel {
-                            rel == "alternate"
-                        } else {
-                            false
+            if let (Some(link), Some(title), Some(date)) =
+                (
+                    match entry
+                        .links
+                        .iter()
+                        .find(|l| {
+                            if let Some(rel) = &l.rel {
+                                rel == "alternate"
+                            } else {
+                                false
+                            }
+                        })
+                        .map(|l| &l.href)
+                    {
+                        Some(s) => match Url::parse(s) {
+                            Ok(u) => Some(u),
+                            Err(ParseError::RelativeUrlWithoutBase) => {
+                                Url::parse(&format!("{}{}", url.origin().ascii_serialization(), &s))
+                                    .ok()
+                            }
+                            Err(_) => None,
+                        },
+                        None => {
+                            // If an alternate link is missing just grab one of them
+                            match entry.links.clone().into_iter().next().map(|l| l.href) {
+                                Some(s) => match Url::parse(&s) {
+                                    Ok(u) => Some(u),
+                                    Err(ParseError::RelativeUrlWithoutBase) => Url::parse(
+                                        &format!("{}{}", url.origin().ascii_serialization(), &s),
+                                    )
+                                    .ok(),
+                                    Err(_) => None,
+                                },
+                                None => return Err(OpenringError::FeedBadTitle(url.to_string())),
+                            }
                         }
-                    })
-                    .map(|l| &l.href)
-                {
-                    Some(s) => match Url::parse(s) {
-                        Ok(u) => Some(u),
-                        Err(ParseError::RelativeUrlWithoutBase) => {
-                            Url::parse(&format!("{}{}", url, &s)).ok()
-                        }
-                        Err(_) => None,
                     },
-                    None => {
-                        // If an alternate link is missing just grab one of them
-                        match entry.links.clone().into_iter().next().map(|l| l.href) {
-                            Some(s) => match Url::parse(&s) {
-                                Ok(u) => Some(u),
-                                Err(ParseError::RelativeUrlWithoutBase) => {
-                                    Url::parse(&format!("{}{}", url, &s)).ok()
-                                }
-                                Err(_) => None,
-                            },
-                            None => return Err(OpenringError::FeedBadTitle(url.to_string())),
-                        }
-                    }
-                },
-                entry.title.as_ref().map(|t| &t.content),
-                entry.published.or(entry.updated),
-            ) {
+                    entry.title.as_ref().map(|t| &t.content),
+                    entry.published.or(entry.updated),
+                )
+            {
                 // Skip articles after args.before, if present
                 if let Some(before) = args.before {
                     if date.date_naive() > before {