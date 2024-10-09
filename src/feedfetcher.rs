lukehsiao/openring-rs

commit f1db30995a0df6870c24d3bedc0b9052150d0b1f

refactor: switch to cargo-style progress
It's kind of nice to see the actual list of URLs, since the compact
version means you rarely see some URLs. It's also more explicit if there
were errors, without needing to turn on verbosity.

Signed-off-by: Luke Hsiao <luke@hsiao.dev>

src/lib.rs
@@ -20,6 +20,7 @@ use tera::Tera;
 use tokio::task::JoinSet;
 use tracing::{debug, info, warn};
 use url::{ParseError, Url};
+use yansi::Paint;
 
 use crate::{
     args::Args,
@@ -76,8 +77,10 @@ fn parse_urls_from_file(path: &Path) -> Result<Vec<Url>> {
 // Skips feeds if there are errors. Shows progress.
 async fn get_feeds_from_urls(urls: &[Url], cache: &Arc<Cache>) -> Vec<(Feed, Url)> {
     let pb = ProgressBar::new(urls.len() as u64).with_style(
-        ProgressStyle::with_template("{spinner} [{human_pos}/{human_len}] [{bar}] {msg}").unwrap(),
+        ProgressStyle::with_template("{prefix:>10} [{bar}] {human_pos}/{human_len}: {msg}")
+            .unwrap(),
     );
+    pb.set_prefix("Fetching".bold().to_string());
 
     let mut join_set = JoinSet::new();
 
@@ -90,9 +93,16 @@ async fn get_feeds_from_urls(urls: &[Url], cache: &Arc<Cache>) -> Vec<(Feed, Url
 
     while let Some(result) = join_set.join_next().await {
         pb.inc(1);
-        if let Ok(Ok((feed, url))) = result {
-            pb.set_message(format!("{url}"));
-            feeds.push((feed, url));
+        match result {
+            Ok(Ok((feed, url))) => {
+                pb.set_message(format!("{url}"));
+                pb.println(format!("{:>10} {url}", "Fetched".bold().green()));
+                feeds.push((feed, url));
+            }
+            Ok(Err(e)) => {
+                pb.set_message(format!("{:>10} {e}", "Error".bold().red()));
+            }
+            _ => (),
         }
     }
 

Cargo.toml
@@ -34,3 +34,4 @@ tokio = { version = "1.40.0", features = ["full"] }
 tracing = "0.1.40"
 tracing-subscriber = { version = "0.3.18", features = ["env-filter", "fmt"] }
 url = { version = "2.5.2", features = ["serde"] }
+yansi = { version = "1.0.1", features = ["detect-tty", "detect-env"] }

commit dcd0b6b7b46005994b5d49525b6af35995293c17

refactor: print error progress correctly
This fixes the printing of error lines to match what I intended in the
original commit.

Fixes: f1db30995a0d ("refactor: switch to cargo-style progress")

src/feedfetcher.rs
@@ -18,12 +18,12 @@ use crate::{
 
 pub(crate) trait FeedFetcher {
     /// Fetch a feed
-    async fn fetch_feed(&self, cache: &Arc<Cache>) -> Result<(Feed, Url), OpenringError>;
+    async fn fetch_feed(&self, cache: &Arc<Cache>) -> Result<Feed, OpenringError>;
 }
 
 impl FeedFetcher for Url {
     /// Fetch a feed for a URL
-    async fn fetch_feed(&self, cache: &Arc<Cache>) -> Result<(Feed, Url), OpenringError> {
+    async fn fetch_feed(&self, cache: &Arc<Cache>) -> Result<Feed, OpenringError> {
         let client: Client = ClientBuilder::new()
             .timeout(Duration::from_secs(30))
             .user_agent(concat!(crate_name!(), '/', crate_version!()))
@@ -41,7 +41,7 @@ impl FeedFetcher for Url {
                         // TODO: This is just copy-pasted, should be reused
                         if let Some(ref feed_str) = cv.body {
                             return match parser::parse(feed_str.as_bytes()) {
-                                Ok(feed) => Ok((feed, self.clone())),
+                                Ok(feed) => Ok(feed),
                                 Err(e) => {
                                     warn!(
                                         url=%self.as_str(),
@@ -163,7 +163,7 @@ impl FeedFetcher for Url {
 
         match body {
             Ok(feed_str) => match parser::parse(feed_str.as_bytes()) {
-                Ok(feed) => Ok((feed, self.clone())),
+                Ok(feed) => Ok(feed),
                 Err(e) => {
                     warn!(
                         url=%self.as_str(),

src/lib.rs
@@ -77,8 +77,7 @@ fn parse_urls_from_file(path: &Path) -> Result<Vec<Url>> {
 // Skips feeds if there are errors. Shows progress.
 async fn get_feeds_from_urls(urls: &[Url], cache: &Arc<Cache>) -> Vec<(Feed, Url)> {
     let pb = ProgressBar::new(urls.len() as u64).with_style(
-        ProgressStyle::with_template("{prefix:>10} [{bar}] {human_pos}/{human_len}: {msg}")
-            .unwrap(),
+        ProgressStyle::with_template("{prefix:>8} [{bar}] {human_pos}/{human_len}: {msg}").unwrap(),
     );
     pb.set_prefix("Fetching".bold().to_string());
 
@@ -87,20 +86,23 @@ async fn get_feeds_from_urls(urls: &[Url], cache: &Arc<Cache>) -> Vec<(Feed, Url
     for url in urls {
         let cache_clone = Arc::clone(cache);
         let url_clone = url.clone();
-        join_set.spawn(async move { url_clone.fetch_feed(&cache_clone).await });
+        join_set.spawn(async move {
+            let fetch_result = url_clone.fetch_feed(&cache_clone).await;
+            (url_clone, fetch_result)
+        });
     }
     let mut feeds = Vec::new();
 
     while let Some(result) = join_set.join_next().await {
         pb.inc(1);
         match result {
-            Ok(Ok((feed, url))) => {
+            Ok((url, Ok(feed))) => {
                 pb.set_message(format!("{url}"));
-                pb.println(format!("{:>10} {url}", "Fetched".bold().green()));
+                pb.println(format!("{:>8} {url}", "Fetched".bold().green()));
                 feeds.push((feed, url));
             }
-            Ok(Err(e)) => {
-                pb.set_message(format!("{:>10} {e}", "Error".bold().red()));
+            Ok((url, Err(e))) => {
+                pb.println(format!("{:>8} {url} ({e})", "Error".bold().red()));
             }
             _ => (),
         }